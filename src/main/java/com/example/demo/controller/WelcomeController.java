c67c484a6bf4625819dfbd2ac1d90d4c32fd020f
satish <satishchekkapalli@gmail.com>
2025-02-11 20:05:35 +0530

    first commit

---
+++ src/main/java/com/example/demo/controller/WelcomeController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/welcome")
+public class WelcomeController {
+	
+	@GetMapping
+	public String welcome() {
+		System.out.println("Azure service working");
+		return "Azure service working";
+	}
+
+}
